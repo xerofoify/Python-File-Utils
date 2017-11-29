59740cec32f58aaf0ede1de3ef1c7b007bf74806
Nicholas Krause <xerofoify@gmail.com>
2017-11-29 13:58:55 -0500

    Add functions for SHA1 and MD5 checksums
    This adds the functions to pass a absolute path and convert to
    a relative one as related to this program's working directory
    at runtime. After depending on function called return either
    a md5 checksum object or sha1 respectively.

--- core.py
+++ core.py
@@ -1,7 +1,22 @@
 import os
+import hashlib
 ## This takes a passed path and
 ## returns a relative path from it
 ## as it relates to working directory
 def getRelPath(path):
-	path = os.path.relpath(path);
+	path = os.path.relpath(path)
+	return path
+## Return a md5 checksum object after 
+## usinf getRelPath to convert the path
+## into a relative path as related to
+## the current working directory
+def getMd5(path):
+	path = getRelPath(path)
+	checksum = md5.new(path)
 	return path;
+## Same as the above function but
+## for SHA1 hashes instead
+def getSHA1(path):
+	path = getRelPath(path)	
+	checksum = sha1.new(path);
+	return checksum
