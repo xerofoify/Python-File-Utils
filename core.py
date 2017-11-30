1a00b0ab0d3b64f123d5a3e0045f4e9708901a99
Marvin <marvin_sanchez@live.com>
2017-11-29 21:52:08 -0500

    Fix #4: Added code to the file size

--- core.py
+++ core.py
@@ -6,7 +6,7 @@ import hashlib
 def getRelPath(path):
 	path = os.path.relpath(path)
 	return path
-## Return a md5 checksum object after 
+## Return a md5 checksum object after
 ## usinf getRelPath to convert the path
 ## into a relative path as related to
 ## the current working directory
@@ -17,6 +17,11 @@ def getMd5(path):
 ## Same as the above function but
 ## for SHA1 hashes instead
 def getSHA1(path):
-	path = getRelPath(path)	
+	path = getRelPath(path)
 	checksum = sha1.new(path);
 	return checksum
+## This takes a passed path and
+## returns a file size in bytes
+def getSize(path):
+    path = os.stat(path)
+    return path.st_size
