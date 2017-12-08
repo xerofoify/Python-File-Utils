957176b8d58b52d80e5a57c43f1ecc0750fd6b3e
Nicholas Krause <xerofoify@gmail.com>
2017-12-08 10:45:00 -0500

    Fix tests

---
+++ python/test_lib.py
@@ -0,0 +1,4 @@
+import core
+import getFileSize
+def test_getFileSize1():
+assert getFileSize.getFileSize("file.txt") == 43
--- python/tests.py
+++
@@ -1,2 +0,0 @@
-def test_getFileSize1():
-assert File_Manipy.getFileSize("file.txt") == 43
